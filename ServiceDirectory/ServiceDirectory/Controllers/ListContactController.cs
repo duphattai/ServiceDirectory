422975498ceb1da98f37e439c784cf9a9bdc8a9d
Nhan <nguyenkhoaminhnhan@gmail.com>
2015-08-27 11:48:00 +0700

    Layout simple contact

---
+++ ServiceDirectory/ServiceDirectory/Controllers/ListContactController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServiceDirectory.Controllers
+{
+    public class ListContactController : Controller
+    {
+        //
+        // GET: /ListContact/
+
+        public ActionResult Index()
+        {
+            return PartialView("ListContact");
+        }
+
+    }
+}
