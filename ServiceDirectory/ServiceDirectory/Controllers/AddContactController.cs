7a77cf6adb706d3d46f5361bea49b05edb860ad4
Nhan <nguyenkhoaminhnhan@gmail.com>
2015-08-27 22:36:11 +0700

    Create - Edit Contact layout

---
+++ ServiceDirectory/ServiceDirectory/Controllers/AddContactController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServiceDirectory.Controllers
+{
+    public class AddContactController : Controller
+    {
+        //
+        // GET: /AddContact/
+
+        public ActionResult Index()
+        {
+            return PartialView("AddContact");
+        }
+
+    }
+}
